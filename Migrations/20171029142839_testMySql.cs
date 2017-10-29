ff1cd484437d6ccb3eaab73229590343dc76bdb9
sofysmo <sov-95@mail.ru>
2017-10-29 17:30:08 +0300

    added DB

---
+++ Migrations/20171029142839_testMySql.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KriptoFeet.Migrations
+{
+    public partial class testMySql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SourceInfos",
+                columns: table => new
+                {
+                    SourceInfoId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: true),
+                    Name = table.Column<string>(type: "longtext", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SourceInfos", x => x.SourceInfoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataEventRecords",
+                columns: table => new
+                {
+                    DataEventRecordId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: true),
+                    Name = table.Column<string>(type: "longtext", nullable: true),
+                    SourceInfoId = table.Column<long>(type: "bigint", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataEventRecords", x => x.DataEventRecordId);
+                    table.ForeignKey(
+                        name: "FK_DataEventRecords_SourceInfos_SourceInfoId",
+                        column: x => x.SourceInfoId,
+                        principalTable: "SourceInfos",
+                        principalColumn: "SourceInfoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataEventRecords_SourceInfoId",
+                table: "DataEventRecords",
+                column: "SourceInfoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataEventRecords");
+
+            migrationBuilder.DropTable(
+                name: "SourceInfos");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using KriptoFeet.DB;
+using KriptoFeet.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
 
 namespace KriptoFeet
 {
@@ -21,12 +27,29 @@ namespace KriptoFeet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            var sqlConnectionString = Configuration.GetConnectionString("DataAccessMySqlProvider");
+
+            services.AddDbContext<DomainModelMySqlContext>(options =>
+                options.UseMySql(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("KriptoFeet")
+                )
+            );
+
+            services.AddScoped<IDataAccessProvider, DataAccessMySqlProvider>();
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
