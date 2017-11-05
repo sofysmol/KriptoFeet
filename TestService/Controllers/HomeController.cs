a4dc502286eaf4e8a285ac5bd7f9f37a83f33089
sofysmo <sov-95@mail.ru>
2017-11-05 15:11:12 +0300

    KF-5 added Category Page

--- TestService/Controllers/HomeController.cs
+++ TestService/Controllers/HomeController.cs
@@ -15,6 +15,13 @@ namespace KriptoFeet.Controllers
             return View();
         }
 
+        public IActionResult Сategory()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
